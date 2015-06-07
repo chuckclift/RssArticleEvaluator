4e8964034ec1193060cb25845004a4649301e35e
chuckclift <cclift119@gmail.com>
2015-06-07 12:44:24 -0400

    added data normalization

--- evaluator.py
+++ evaluator.py
@@ -1,3 +1,4 @@
+﻿from __future__ import division
 import time
 import re
 
@@ -8,18 +9,18 @@ classifierWords = filter(None, classifierWords)
 classifierWords = [word.strip() for word in classifierWords]
 classifierFile.close()
 
-
 articleFile = open("articles.txt", encoding="utf8")
 articles = articleFile.read()
 articleFile.close()
 
+
+
 LOWEST_GRADE = 1
 HIGHEST_GRADE = 5
+MAX_WORD_COUNT = 20
+
 
 class Article(object):
-    
-
-
     def __init__(self, searchKeywords, thisArticleText):
         self.searchKeywords = searchKeywords
         self.articleText = thisArticleText.replace("  ", "")
@@ -29,10 +30,8 @@ class Article(object):
             count = 0
             regex = "[ \.\?\"\'!,]" + word + "[ \.\?\"\'!,s]"
             count = len(re.findall(regex, self.articleText.lower()))
-            self.keywordFrequencies.append(count)
-            
-
-
+            normalizedScore = count / MAX_WORD_COUNT
+            self.keywordFrequencies.append(normalizedScore)
 
     def getText(self):
         return self.articleText
@@ -41,38 +40,36 @@ class Article(object):
         csv = ""
         for value in self.keywordFrequencies:
             csv = csv + str(value) + ","
-            
+
         return csv
 
+
 def makeCsv(articleObjList):
-    
     finalCsv = ""
     counter = 0
-
 
     for articleOb in articleObjList:
 
         current = articleOb.getText()
         articleLength = len(current)
-  
+
         while len(current) > 0:
-        ##prints out one line for the user to read
+            # prints out one line for the user to read
             print(current[:70])
             current = current[70:]
 
-
-        ##every fifth line, it stops
-        ##and waits for the user to read
+            # every fifth line, it stops
+            # and waits for the user to read
             counter += 1
             if counter is 5:
                 counter = 0
-                var = input("")
-            ##user presses enter to continue reading
+                input("")
+                # user presses enter to continue reading
 
-    ##after the user is done reading, he is asked
-    ##to assign a category.  It won't let him submit
-    ##an empty string so he doesn't accidentally
-    ##skip this part
+                # after the user is done reading, he is asked
+                # to assign a category.  It won't let him submit
+                # an empty string so he doesn't accidentally
+                # skip this part
         grade = ""
 
         question = ("Grade?("
@@ -80,7 +77,7 @@ def makeCsv(articleObjList):
                     + "-"
                     + str(HIGHEST_GRADE)
                     + ")\n")
-        
+
         while len(grade) is 0:
 
             grade = input(question)
@@ -89,53 +86,40 @@ def makeCsv(articleObjList):
                 if int(grade) < LOWEST_GRADE:
                     grade = ""
                     print("Your number is too low\n")
-                elif  int(grade) > HIGHEST_GRADE:
+                elif int(grade) > HIGHEST_GRADE:
                     grade = ""
                     print("Your number is too high\n")
 
         finished = input("Done?(y/n)\n")
 
-        finalCsv = finalCsv + articleOb.reportData() + grade + "\n"
+        # Normalizing the data on the scale of zero to one
+        grade = (int(grade) - LOWEST_GRADE) / (HIGHEST_GRADE - LOWEST_GRADE)
+
+        finalCsv = finalCsv + articleOb.reportData() + str(grade) + "\n"
 
         if finished == "y":
             return finalCsv
-        
+
     return finalCsv
-    
-        
 
+# cleans up the formatting, making it more readable
+articles = articles.replace("<article>", "")
+articles = articles.replace("   ", "")
+articles = articles.replace("\n", " ")
 
-##cleans up the formatting, making it
-##more readable
-articles = articles.replace("<article>","")
-articles = articles.replace("   ","")
-articles = articles.replace("\n"," ")
-
-##getting rid of the space after the last
-##article
+# getting rid of the space after the last article
 last = articles.rfind("</article")
 articles = articles[:last]
 
 
-##splitting articles into a list for processing
-##in the upcoming loop
+# splitting articles into a list for processing in the upcoming loop
 articleTextList = articles.split("</article>")
 articleObjects = []
 
-
 for workingArticle in articleTextList:
-    current = Article(classifierWords, workingArticle)
-    articleObjects.append(current)
+    currentArticle = Article(classifierWords, workingArticle)
+    articleObjects.append(currentArticle)
 
-
-        
-
-outputCsv = open("csvData.txt","w")
+outputCsv = open("csvData.txt", "w")
 outputCsv.write(makeCsv(articleObjects))
 outputCsv.close()
-
-
-
-
-
-
