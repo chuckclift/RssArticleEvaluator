3e4a977d1aa5a50966b072c4a9aca5005e0a2b47
chuckclift <cclift119@gmail.com>
2015-06-08 13:12:04 -0400

    added if name == main

--- evaluator.py
+++ evaluator.py
@@ -2,17 +2,6 @@
 import re
 
 
-
-with open("classifierWords.txt") as f:
-    classifierWords = f.read()
-    classifierWords = classifierWords.split("\n")
-    classifierWords = filter(None, classifierWords)
-    classifierWords = [word.strip() for word in classifierWords]
-
-with open("articles.txt") as g:
-    articles = g.read()
-
-
 LOWEST_GRADE = 1
 HIGHEST_GRADE = 5
 MAX_WORD_COUNT = 20
@@ -23,9 +12,9 @@ class Article(object):
         self.keywords = keywords
         self.article_text = article_text.replace("  ", "")
         self.keywordFrequencies = []
+        self.score = 0.0
 
         for word in keywords:
-            count = 0
             regex = "[ \.\?\"\'!,]" + word + "[ \.\?\"\'!,s]"
             count = len(re.findall(regex, self.article_text.lower()))
             normalized_score = count / MAX_WORD_COUNT
@@ -34,24 +23,27 @@ class Article(object):
     def get_text(self):
         return self.article_text
 
+    def set_score(self, score_value):
+        self.score = score_value
+
     def report_data(self):
         csv = ""
         for value in self.keywordFrequencies:
             csv = csv + str(value) + ","
+        csv = csv + str(self.score) + "\n"
 
         return csv
 
 
-def make_csv(article_object_list):
-    final_csv = ""
+def score_articles(article_object_list):
     counter = 0
+    scored_articles = []
 
     for articleOb in article_object_list:
 
         current = articleOb.get_text()
 
         while len(current) > 0:
-
             # prints out one line for the user to read
             try:
                 print(current[:70])
@@ -92,40 +84,53 @@ def make_csv(article_object_list):
                     grade = ""
                     print("Your number is too high\n")
 
-        finished = input("Done?(y/n)\n")
-
         # Normalizing the data on the scale of zero to one
         grade = (int(grade) - LOWEST_GRADE) / (HIGHEST_GRADE - LOWEST_GRADE)
 
-        final_csv = final_csv + articleOb.report_data() + str(grade) + "\n"
+        articleOb.set_score(grade)
+        scored_articles.append(articleOb)
+
+        finished = input("Done?(y/n)\n")
 
         if finished == "y":
-            return final_csv
+            return scored_articles
 
-    return final_csv
-
-# cleans up the formatting, making it more readable
-articles = articles.replace("<article>", "")
-articles = articles.replace("   ", "")
-articles = articles.replace("\n", " ")
-
-# getting rid of the space after the last article
-last = articles.rfind("</article")
-articles = articles[:last]
-
-
-# splitting articles into a list for processing in the upcoming loop
-articleTextList = articles.split("</article>")
-articleObjects = []
-
-for workingArticle in articleTextList:
-    currentArticle = Article(classifierWords, workingArticle)
-    articleObjects.append(currentArticle)
-
-endCsv = make_csv(articleObjects)
-
-with open("csvData.txt", "w") as f:
-    f.write(endCsv)
+    return scored_articles
 
 
 
+
+
+if __name__ == "__main__":
+    with open("classifierWords.txt") as f:
+        classifierWords = f.read()
+        classifierWords = classifierWords.split("\n")
+        classifierWords = filter(None, classifierWords)
+        classifierWords = [word.strip() for word in classifierWords]
+
+    with open("articles.txt") as g:
+        articles = g.read()
+
+    # cleans up the formatting, making it more readable
+    articles = articles.replace("<article>", "")
+    articles = articles.replace("   ", "")
+    articles = articles.replace("\n", " ")
+
+    # getting rid of the space after the last article
+    last = articles.rfind("</article")
+    articles = articles[:last]
+
+    # splitting articles into a list for processing in the upcoming loop
+    articleTextList = articles.split("</article>")
+    articleObjects = []
+
+    for workingArticle in articleTextList:
+        currentArticle = Article(classifierWords, workingArticle)
+        articleObjects.append(currentArticle)
+
+    graded_articles = score_articles(articleObjects)
+
+    print("Writing csv to file csvData.csv")
+    with open("csvData.csv", "w") as f:
+        for a in graded_articles:
+            f.write(a.report_data())
